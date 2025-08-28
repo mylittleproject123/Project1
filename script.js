909815436a9005be0616815b6aeedabbe2733b8d
mylittleproject123 <mylittleproject123@proton.me>
2025-08-28 19:22:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2399,7 +2399,8 @@ document.addEventListener('DOMContentLoaded', () => {
     updateFooterFromBusinessAddress();
 
 
+
 document.querySelector('form').addEventListener('submit', (event) => {
     event.preventDefault(); // stop the default form submission (which reloads the page)
 });
-   
+
