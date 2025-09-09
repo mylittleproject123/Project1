45ef311544f847cb7cc5e80c66049fd30b2fb334
mylittleproject123 <mylittleproject123@proton.me>
2025-09-09 21:27:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2411,9 +2411,6 @@ document.addEventListener('DOMContentLoaded', () => {
     currentCountry = savedCountry;
     updateFooterFromBusinessAddress();
 });
-
-});
-
 document.querySelector('form').addEventListener('submit', (event) => {
     event.preventDefault(); // stop the default form submission (which reloads the page)
     
