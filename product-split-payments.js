99b1edcef2874d6bd4dd2d0597bbcd079e580f4e
mylittleproject123 <mylittleproject123@proton.me>
2025-09-09 12:00:24 +0200

    Create product-split-payments.js

---
+++ product-split-payments.js
@@ -0,0 +1,49 @@
+const productsForSplitPayment = [
+    {
+        id: 'iphone16promax',
+        name: 'iPhone 16 Pro Max',
+        variants: [
+            { storage: '256GB', price: 769 },
+            { storage: '512GB', price: 869 },
+            { storage: '1TB', price: 969 },
+        ],
+        image: 'https://m.media-amazon.com/images/I/61UMlmDXG+L._AC_SX466_.jpg'
+    },
+    {
+        id: 'iphone15promax',
+        name: 'iPhone 15 Pro Max',
+        variants: [
+            { storage: '256GB', price: 629 },
+            { storage: '512GB', price: 729 },
+            { storage: '1TB', price: 829 },
+        ],
+        image: 'https://m.media-amazon.com/images/I/61v5Jay9F5L._AC_SX569_.jpg'
+    },
+    {
+        id: 'galaxys25ultra',
+        name: 'Samsung Galaxy S25 Ultra',
+        variants: [
+            { storage: '256GB', price: 719 },
+            { storage: '512GB', price: 819 },
+            { storage: '1TB', price: 919 },
+        ],
+        image: 'https://m.media-amazon.com/images/I/61n0lmxP5-L._AC_SX569_.jpg'
+    },
+    {
+        id: 'galaxys24ultra',
+        name: 'Samsung Galaxy S24 Ultra',
+        variants: [
+            { storage: '256GB', price: 599 },
+            { storage: '512GB', price: 699 },
+        ],
+        image: 'https://m.media-amazon.com/images/I/51E3rux4DgL.__AC_SX300_SY300_QL70_FMwebp_.jpg'
+    },
+    {
+        id: 'airpods4',
+        name: 'AirPods 4th Generation',
+        variants: [
+            { storage: 'Standard', price: 129 }
+        ],
+        image: 'https://m.media-amazon.com/images/I/61df2M5+OnL._AC_SX466_.jpg'
+    }
+];
