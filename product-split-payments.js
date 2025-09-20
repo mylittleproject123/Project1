8dfc3b1d76d33d145e6abcf847dbd520df89de95
mylittleproject123 <mylittleproject123@proton.me>
2025-09-20 11:02:11 +0200

    Update product-split-payments.js

--- product-split-payments.js
+++ product-split-payments.js
@@ -3,9 +3,9 @@ const productsForSplitPayment = [
         id: 'iphone16promax',
         name: 'iPhone 16 Pro Max',
         variants: [
-            { storage: '256GB', price: 769 },
-            { storage: '512GB', price: 869 },
-            { storage: '1TB', price: 969 },
+            { storage: '256GB', price: 799 },
+            { storage: '512GB', price: 849 },
+            { storage: '1TB', price: 920 },
         ],
         colors: ['Natural Titanium', 'Blue Titanium', 'White Titanium', 'Black Titanium'],
         image: 'https://m.media-amazon.com/images/I/61UMlmDXG+L._AC_SX466_.jpg'
@@ -14,8 +14,8 @@ const productsForSplitPayment = [
         id: 'iphone15promax',
         name: 'iPhone 15 Pro Max',
         variants: [
-            { storage: '256GB', price: 629 },
-            { storage: '512GB', price: 729 },
+            { storage: '256GB', price: 699 },
+            { storage: '512GB', price: 759 },
             { storage: '1TB', price: 829 },
         ],
         colors: ['Natural Titanium', 'Blue Titanium', 'White Titanium', 'Black Titanium'],
@@ -37,7 +37,7 @@ const productsForSplitPayment = [
         name: 'Samsung Galaxy S24 Ultra',
         variants: [
             { storage: '256GB', price: 599 },
-            { storage: '512GB', price: 699 },
+            { storage: '512GB', price: 624 },
         ],
         colors: ['Titanium Gray', 'Titanium Black', 'Titanium Violet', 'Titanium Yellow'],
         image: 'https://m.media-amazon.com/images/I/51E3rux4DgL.__AC_SX300_SY300_QL70_FMwebp_.jpg'
