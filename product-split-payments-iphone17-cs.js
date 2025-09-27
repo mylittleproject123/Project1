863b7b38df84867a7297c103e37ec38a629bacfb
mylittleproject123 <mylittleproject123@proton.me>
2025-09-27 21:27:07 +0200

    Create product-split-payments-iphone17-cs.js

---
+++ product-split-payments-iphone17-cs.js
@@ -0,0 +1,44 @@
+const productsForSplitPaymentIphone17 = [
+    {
+        id: 'iphone17',
+        name: 'iPhone 17',
+        variants: [
+            { storage: '128GB', price: 1099 },
+            { storage: '256GB', price: 1199 },
+        ],
+        colors: ['Polární záře', 'Vesmírná černá', 'Pouštní zlatá'],
+        image: 'https://citymagazine.b-cdn.net/wp-content/uploads/2025/08/hero-series-iphone17-2025-0-1400x933.webp'
+    },
+    {
+        id: 'iphone17plus',
+        name: 'iPhone 17 Plus',
+        variants: [
+            { storage: '128GB', price: 1199 },
+            { storage: '256GB', price: 1299 },
+        ],
+        colors: ['Polární záře', 'Vesmírná černá', 'Pouštní zlatá'],
+        image: 'https://citymagazine.b-cdn.net/wp-content/uploads/2025/08/hero-series-iphone17-2025-0-1400x933.webp'
+    },
+    {
+        id: 'iphone17pro',
+        name: 'iPhone 17 Pro',
+        variants: [
+            { storage: '256GB', price: 1399 },
+            { storage: '512GB', price: 1599 },
+            { storage: '1TB', price: 1799 },
+        ],
+        colors: ['Titanová černá', 'Přírodní titan', 'Pouštní titan'],
+        image: 'https://www.apple.com/v/iphone-15-pro/c/images/overview/design/design_display_pro_large.jpg'
+    },
+    {
+        id: 'iphone17promax',
+        name: 'iPhone 17 Pro Max',
+        variants: [
+            { storage: '256GB', price: 1499 },
+            { storage: '512GB', price: 1699 },
+            { storage: '1TB', price: 1899 },
+        ],
+        colors: ['Titanová černá', 'Přírodní titan', 'Pouštní titan'],
+        image: 'https://www.apple.com/v/iphone-15-pro/c/images/overview/design/design_display_pro_max_large.jpg'
+    }
+];
